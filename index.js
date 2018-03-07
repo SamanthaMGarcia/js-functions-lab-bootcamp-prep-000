44dc0488aa7304d579248d26ec659fbcf9b63634
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-07 15:43:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function happyHolidays(string) {
  return "Happy holidays!"
 }
+<<<<<<< HEAD
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
@@ -10,3 +11,8 @@ function happyHolidayTo(holiday,name) {
 function holidayCountdown(holiday,days) {
   return `It's ${days} days until ${holiday}!`
 }
+=======
+function happyHolidaysTo(string) {
+  return "Happy holidays, string!"
+}
+>>>>>>> 9df71b32ff58ef1535a7941b42c35fde7ba9d3d7
